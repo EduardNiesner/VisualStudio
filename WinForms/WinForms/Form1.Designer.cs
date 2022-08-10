f67ad8974a1ed98afcede11f8e8f0934c364f68a
Eduard Niesner <eduard.niesner@brooksautomation.com>
2022-08-10 11:00:39 +0300

    - added a new button called "Feature E"

--- WinForms/WinForms/Form1.Designer.cs
+++ WinForms/WinForms/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace WinForms
         private void InitializeComponent()
         {
             this.btnMaster = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnMaster
@@ -42,11 +43,22 @@ namespace WinForms
             this.btnMaster.UseVisualStyleBackColor = true;
             this.btnMaster.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 143);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Feature E";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(817, 723);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnMaster);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@ namespace WinForms
         #endregion
 
         private System.Windows.Forms.Button btnMaster;
+        private System.Windows.Forms.Button button1;
     }
 }
 
