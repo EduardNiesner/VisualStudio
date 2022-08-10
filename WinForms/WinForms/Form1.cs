d845b5f59011fd5556f01ede02b43f6229cadb5e
Eduard Niesner <eduard.niesner@brooksautomation.com>
2022-08-10 10:23:09 +0300

    - renamed button 1 to master

--- WinForms/WinForms/Form1.cs
+++ WinForms/WinForms/Form1.cs
@@ -19,7 +19,7 @@ namespace WinForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Button 1 was pressed.");
+            MessageBox.Show("Master was pressed.");
         }
     }
 }
